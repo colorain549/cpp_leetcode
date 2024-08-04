9ad5a8e4c65cc048c07b2cbd392402f1ab1e6a10
Lam <colorain@live.com>
2024-08-04 12:22:59 +0800

    add 4_string 1_reverseString and modified README.md

---
+++ 4_string/1_reverseString3.cc
@@ -0,0 +1,45 @@
+// 输出
+// o l l e h 
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::swap;
+using std::vector;
+
+#include <vector>
+
+class Solution {
+public:
+    // Custom swap function
+    void exchange(char& a, char& b) {
+        a ^= b;
+        b ^= a;
+        a ^= b;
+    }
+
+    void reverseString(std::vector<char>& s) {
+        int n = s.size();
+        for (int i = 0, j = n - 1; i < j; i++, j--) {
+            exchange(s[i], s[j]);
+        }
+    }
+};
+
+int main()
+{
+    vector<char> s = {'h', 'e', 'l', 'l', 'o'};
+
+    Solution solution;
+    solution.reverseString(s);
+
+    for (auto &elem : s)
+    {
+        cout << elem << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
